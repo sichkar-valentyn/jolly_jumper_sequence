acd3570cbbe7e0cba94f83c848e4ba5e08f65781
Valentyn N Sichkar <34546286+sichkar-valentyn@users.noreply.github.com>
2018-07-27 15:19:31 +0300

    Add files via upload

---
+++ jolly_jumper.py
@@ -0,0 +1,30 @@
+# Implementing the task
+# Checking if the input numbers are jolly jumper
+# Jolly jumper is a sequence that takes all possible values from 1 to n-1
+# From input neighbour elements by subtracting them
+# For example input sequence 1 -3 -4 1 -1
+# is jolly jumper, because result is the sequence 4 1 3 2
+
+# Creating a list with integer numbers by inputting them in one line with gap
+lst = [int(i) for i in input().split()]
+
+# Going through the elements of the list
+# Creating new list of elements that are subtraction of neighbour elements
+lst_new = []
+for i in range(len(lst) - 1):
+    lst_new += [abs(lst[i] - lst[i+1])]
+
+# Going through the elements of the new list
+# Checking if they are from the sequence from 1 to n-1
+n = 1
+c = True
+for x in lst_new:
+    if n in lst_new:
+        n += 1
+    else:
+        print('Not jolly')
+        c = False
+        break
+
+if c:
+    print('Jolly')
